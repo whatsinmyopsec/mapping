cfd04e16a769a90912a4ba66970f85fa2ef9c0e4
Kevin <kevin.power.97.kp@gmail.com>
2020-01-05 22:15:03 +0000

    iteration 1

---
+++ index.js
@@ -0,0 +1,25 @@
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
+const screen = blessed.screen();
+const fs = require('fs');
+const csv = require('csv-parser');
+const map = contrib.map({label: 'World Map'});
+
+screen.append(map);
+
+const filepath = process.argv[2];
+
+fs.createReadStream(filepath)
+    .on('error', () => {
+        // handle error
+    })
+    .pipe(csv())
+    .on('data', (row) => {
+        // console.log(row.lat,row.lon);
+        map.addMarker({'lat': row.lat, 'lon': row.lon, color: 'red', char: 'X'});
+    })
+    .on('end', () => {
+        screen.render();
+        // handle end of CSV
+    });
+
