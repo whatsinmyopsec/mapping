e1f144920a74d101c597587bc4242d967f6c81e5
Kevin <kevin.power.97.kp@gmail.com>
2020-04-22 03:15:00 +0100

    cleaning out the trash

--- graphql-mongo-server/src/models/Ip.js
+++
@@ -1,3 +0,0 @@
-import mongoose from "mongoose";
-
-export const Ip = mongoose.model("Ip", { name: String });
--- graphql-mongo-server/src/resolvers.js
+++
@@ -1,7 +0,0 @@
-import { downloads } from "./models/downloads";
-
-export const resolvers = {
-    Query: {
-        src_ip: () => downloads.find()
-    }
-};
